3b52f50bc19ab5da997ea772a925378243b6c49c
samqaguy <74614254+samqaguy@users.noreply.github.com>
2021-12-09 13:47:35 -0500

    Update testInit_api.sh

--- testInit_api.sh
+++ testInit_api.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo Installing Packages
-npm install
+npm install --save
 
 echo Running Jest API Tests..
-jest -t='.*\bAPI\b' --where=local --runInBand
+./node_modules/.bin/jest -t='.*\bAPI\b' --where=local --runInBand
